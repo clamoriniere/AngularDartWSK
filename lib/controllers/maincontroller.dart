640024555fb89ca6527bc3911d1e1f7ed8fbee8f
Cedric Lamorininiere <cedric.lamoriniere@amadeus.com>
2014-10-03 08:33:00 +0200

    Add Menu animation

--- lib/controllers/maincontroller.dart
+++ lib/controllers/maincontroller.dart
@@ -6,9 +6,32 @@ class MainController {
   Scope _scope;
 
   String test = "You!!!";
+  bool _isMenuOpen = false;
 
   MainController(this._http, this._scope) {
 
+  }
+
+  String getBodyClass() {
+    if (_isMenuOpen) {
+      return "open";
+    }
+    return "";
+  }
+
+  String getNavDrawerContainerClass() {
+    if (_isMenuOpen) {
+      return "open opened";
+    }
+    return "";
+  }
+
+  void closeMenu() {
+    _isMenuOpen = false;
+  }
+
+  void toggleMenu() {
+    _isMenuOpen = true;
   }
 
 }
